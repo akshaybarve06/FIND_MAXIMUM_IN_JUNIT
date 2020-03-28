d8780a06431f1f6d048b7a4c841b60ec27c0400b
Akshay Barve <akshaybarve06@gmail.com>
2020-03-28 11:32:04 +0530

    Added: Find Maximum Of Three Integers

---
+++ src/test/java/TestMax.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMax {
+    FindMax maxObject;
+    @Test
+    public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumOne() {
+        maxObject = new FindMax();
+        int integers = maxObject.findMaxInteger(5, 2, 3);
+        Assert.assertEquals(5, integers);
+        System.out.println("Maximum Among All is.."+integers);
+    }
+    @Test
+    public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumTwo() {
+        maxObject = new FindMax();
+        int integers = maxObject.findMaxInteger(12, 44, 23);
+        Assert.assertEquals(4, integers);
+        System.out.println("Maximum Among All is.."+integers);
+    }
+    @Test
+    public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumThree() {
+        maxObject = new FindMax();
+        int integers = maxObject.findMaxInteger(7, 15, 23);
+        Assert.assertEquals(23, integers);
+        System.out.println("Maximum Among All is.."+integers);
+    }
+}
