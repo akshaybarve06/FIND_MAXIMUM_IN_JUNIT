9c05df03518f1ec9e47f85a56e358cd430583a5d
Akshay Barve <akshaybarve06@gmail.com>
2020-03-28 13:57:57 +0530

    Added: TC 2.3 Max at Last, Find Maximum Of Three Floats

--- src/test/java/TestMax.java
+++ src/test/java/TestMax.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 public class TestMax {
     FindMax maxObject;
+    //1.1 = Find Max of Three Integers Where Max Integer is first Number
     @Test
     public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumOne() {
         maxObject = new FindMax();
@@ -10,6 +11,7 @@ public class TestMax {
         Assert.assertEquals(5, integers);
         System.out.println("Maximum Among All is.."+integers);
     }
+    //1.2 = Find Max of Three Integers Where Max Integer is Second Number
     @Test
     public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumTwo() {
         maxObject = new FindMax();
@@ -17,6 +19,7 @@ public class TestMax {
         Assert.assertEquals(4, integers);
         System.out.println("Maximum Among All is.."+integers);
     }
+    //1.3 = Find Max of Three Integers Where Max Integer is Third Number
     @Test
     public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumThree() {
         maxObject = new FindMax();
@@ -25,17 +28,27 @@ public class TestMax {
         System.out.println("Maximum Among All is.."+integers);
     }
     @Test
+    //2.1 = Find Max of Three Floats Where Max Integer is First Number
     public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumOne() {
         maxObject = new FindMax();
         Float maxfloatNumber = FindMax.findMaxFloatingNumber(55.36f, 22.1f, 3.54f);
         Assert.assertEquals((Float)55.36f, maxfloatNumber);
         System.out.println("Maximum Among All is.."+maxfloatNumber);
     }
+    //2.2 = Find Max of Three Floats Where Max Integer is Second Number
     @Test
     public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumTwo() {
         maxObject = new FindMax();
         Float maxfloatNumber = FindMax.findMaxFloatingNumber(15.36f, 52.1f, 3.54f);
         Assert.assertEquals((Float)52.1f, maxfloatNumber);
+        System.out.println("Maximum Among All is.."+maxfloatNumber);
+    }
+    //2.3 = Find Max of Three Floats Where Max Integer is Third Number
+    @Test
+    public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumThree() {
+        maxObject = new FindMax();
+        Float maxfloatNumber = FindMax.findMaxFloatingNumber(15.36f, 12.1f, 43.54f);
+        Assert.assertEquals((Float)43.54f, maxfloatNumber);
         System.out.println("Maximum Among All is.."+maxfloatNumber);
     }
 }
