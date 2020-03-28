34cd8df45beb7d88094ae3512e89e2d947bb2b21
Akshay Barve <akshaybarve06@gmail.com>
2020-03-28 16:05:31 +0530

    Added: TC 3.3 Largest at Third, Find Largest Of Three Strings

--- src/test/java/TestMax.java
+++ src/test/java/TestMax.java
@@ -62,5 +62,11 @@ public class TestMax {
         Assert.assertEquals("Strawberry", largestString);
         System.out.println("Largest String Among All is.."+largestString);
     }
-
+    //3.3 = Find Largest of Three Strings Where Largest String is Third
+    @Test
+    public void givenThreeStrings_FindMaxOfThem_ReturnMaximumThree() {
+        String largestString = FindMax.findLargestString("Apple", "Berries", "Strawberry");
+        Assert.assertEquals("Strawberry", largestString);
+        System.out.println("Largest String Among All is.."+largestString);
+    }
 }
